54a119428e822d93dcbdbbe1ca9e751659e0d06c
lschimm <lschimmelfennig@gmail.com>
2019-07-15 15:59:25 -0500

    added get

--- data/accounts/account-router.js
+++ data/accounts/account-router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const knex = require("knex");
 
+const db = require("../dbConfig");
+
 const dbConnection = knex({
   client: "sqlite3",
   connection: {
@@ -8,3 +10,17 @@ const dbConnection = knex({
   },
   useNullAsDefault: true
 });
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  dbConnection("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = server;
