09473097688265bf229ed6d7578a29f4e51d0134
lschimm <lschimmelfennig@gmail.com>
2019-07-15 16:00:21 -0500

    delete implemented

--- data/accounts/account-router.js
+++ data/accounts/account-router.js
@@ -23,4 +23,28 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  dbConnection("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(post => {
+      if (accounts) {
+        res.status(200).json(accounts);
+      } else {
+        res.status(404).json({ message: "account not found" });
+      }
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+router.delete("/:id", (req, res) => {
+  dbConnection("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200({ message: `${account} record(s) deleted` }));
+    })
+    .catch(error => res.status(500).json(error));
+});
+
 module.exports = server;
